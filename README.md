5c1261aef84ffaa4400f36d9d3e5d29c3e9830df
fsantini <francesco.santini@gmail.com>
2013-09-21 10:23:08 +0200

    Initial upload

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ futubox_xmltv
 =============
 
 Downloading EPG for Futubox streaming service
+This program downloads the Electronic Program Guide (EPG) information from the Futubox website and formats them in the XMLTV format to be read by (for example) XBMC.
+
+Requirements:
+
+This program requires a working Python installation (tested with v2.7), python-lxml and python-urllib2.
+
+Configuration:
+
+The program needs to read from the two csv files included in the distribution. You can modify the paths to these files and to the output xml file directly from the futubox_xmltv.py program
+
+Usage:
+
+As a first step, you need to prepare the futubox playlist. Make sure to DISABLE parental control in your futubox settings and then download the m3u playlist as described in the Futubox FAQ here: http://futuboxhd.com/faq/ . If you don't want adult channels you can still remove them from the playlist AFTER the conversion.
+
+Run the script futubox_convert_playlist.py in order to prepare the playlist correctly with the following syntax
+  python futubox_convert_playlist.py <original_playlist.m3u> <output_playlist.m3u>
+
+Run the futubox_xmltv.py script:
+  python futubox_xmltv.py
+
+This script will generate a futubox_guide.xml file.
+
+If you are using XBMC with IPTVSimple plugin, configure the plugin to read the channels from the converted m3u and the EPG from the futubox_guide.xml file.
+
+Known problems:
+
+Ths futubox_xmltv.py script needs to be executed every day, because Futubox only provides channel information one day at a time. You can either run it manually or automate it with a cron job.
+
+The IPTVSimple plugin has troubles displaying the EPG timeline after midnight. I think this is a bug of the plugin.
---
+++ futubox_xmltv.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#    futubox to XMLTV - copyright 2013 Francesco Santini <francesco.santini@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import csv
+#import xml.etree.ElementTree as ET
+from lxml import etree as ET
+import urllib2
+
+# CSV with channel number vs channel names
+chanlistFile = 'futubox_channels.csv'
+# CSV with channel number vs URL number
+chanmapFile = 'futubox_chan_map.csv'
+# output XML guide file
+outXMLFile = 'futubox_guide.xml'
+
+def convertDateTime(datestamp):
+  #converts 2013-09-14T00:30:00+02:00 into 20130914003000 +0200
+  return datestamp.replace('-','').replace(':','').replace('T','').replace('+',' +')
+  
+def getURL(chanID):
+  return 'http://futubox.to/channels/' + str(chanID) + '/programmes.xml'
+  
+# load channel list and generate chan XML
+tv = ET.XML('<?xml version="1.0" encoding="utf-8" ?>\n<tv></tv>')
+chanNames = []
+with open(chanlistFile, 'rb') as csvfile:
+  chanreader = csv.reader(csvfile, delimiter=',')
+  for row in chanreader:
+    chanNames.append(row[1] or '')
+    
+for chanID, chanName in enumerate(chanNames):
+  if chanName != '':
+    chanElement = ET.SubElement(tv, 'channel', {'id': "futubox" + str(chanID+1)})
+    dnameElement = ET.SubElement(chanElement, 'display-name')
+    dnameElement.text = chanName
+
+# load channel IDs
+chanURLs = []
+with open(chanmapFile, 'rb') as csvfile:
+  chanreader = csv.reader(csvfile, delimiter=',')
+  for row in chanreader:
+    chanURLs.append(row[1] or '')
+   
+# read URLs
+for chanID, chanURL in enumerate(chanURLs):
+  if not chanURL: continue
+  print 'Reading', chanURL
+  chanXML = urllib2.urlopen(getURL(chanURL))
+  futuboxXML = ET.parse(chanXML)
+  progs = futuboxXML.find('programmes')
+  for prog in progs.findall('programme'):
+    start = convertDateTime(prog.find('start-at').text)
+    end = convertDateTime(prog.find('end-at').text)
+    title = prog.find('title').text
+    if not title: continue
+    progXMLOut = ET.SubElement(tv, 'programme', {'start': start, 'stop': end, 'channel': "futubox" + str(chanID+1)})
+    progXMLTitle = ET.SubElement(progXMLOut, 'title')
+    progXMLTitle.text = title.strip()
+  chanXML.close()
+  
+with open(outXMLFile, 'wb') as guideXML:
+  guideXML.write(ET.tostring(tv,pretty_print = True,encoding='utf-8',xml_declaration=True))
